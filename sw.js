1a1d733db2db8828caf1fe907f011b5f679b5ba0
hector0722 <80801433+hector0722@users.noreply.github.com>
2026-07-31 11:07:55 -0600

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'programa-consultorio-dental-v4-20260731-drive-discovery-v4';
+const CACHE_NAME = 'programa-consultorio-dental-v4-20260731-mobile-reconnect-v5';
 
 self.addEventListener('install', event => {
   self.skipWaiting();
