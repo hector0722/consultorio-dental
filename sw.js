d43e9b0f808a573cdd0a68835ccfa69ecc3e7e64
hector0722 <80801433+hector0722@users.noreply.github.com>
2026-07-31 15:39:55 -0600

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'programa-consultorio-dental-v4-20260731-photo-upload-v7';
+const CACHE_NAME = 'programa-consultorio-dental-v4-20260731-photo-batch-v8';
 
 self.addEventListener('install', event => {
   self.skipWaiting();
