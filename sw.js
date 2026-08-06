2d542a5160b95e95628680475494c8ad1cbb505d
hector0722 <80801433+hector0722@users.noreply.github.com>
2026-08-06 16:02:58 -0600

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'programa-consultorio-dental-v4-20260806-startup-loader-v14';
+const CACHE_NAME = 'programa-consultorio-dental-v4-20260806-refresh-safety-v15';
 
 self.addEventListener('install', event => {
   self.skipWaiting();
