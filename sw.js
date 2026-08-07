a69802fc6892bad5c8b69b895d69a87ae7eefc87
hector0722 <80801433+hector0722@users.noreply.github.com>
2026-08-06 18:10:31 -0600

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'programa-consultorio-dental-v4-20260806-visit-plan-v21';
+const CACHE_NAME = 'programa-consultorio-dental-v4-20260806-caja-recovery-v22';
 
 self.addEventListener('install', event => {
   self.skipWaiting();
