6eb1012052b44a1c116f244b8f407fba9c0121d9
hector0722 <80801433+hector0722@users.noreply.github.com>
2026-08-07 21:33:06 -0600

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'programa-consultorio-dental-v4-20260807-print-logo-v23';
+const CACHE_NAME = 'programa-consultorio-dental-v4-20260807-payment-receipt-v24';
 
 self.addEventListener('install', event => {
   self.skipWaiting();
